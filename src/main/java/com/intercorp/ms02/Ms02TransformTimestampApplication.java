e3cc8f568ea13315632c165e743d3f9b6d9c37e7
Leonardo <56612664+LeonardoSGSG@users.noreply.github.com>
2025-06-14 05:36:01 -0500

    1st commit

---
+++ src/main/java/com/intercorp/ms02/Ms02TransformTimestampApplication.java
@@ -0,0 +1,13 @@
+package com.intercorp.ms02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms02TransformTimestampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms02TransformTimestampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intercorp/ms02/Ms02TransformTimestampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intercorp.ms02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms02TransformTimestampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
